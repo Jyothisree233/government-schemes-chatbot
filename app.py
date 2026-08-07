fe5fdda7f767fd1a9e00cc58380da9ca4e83cdab
Jyothisree233 <jyothisreelakshmi233@gmail.com>
2026-08-07 18:33:57 +0530

    Fix Railway port binding

--- app.py
+++ app.py
@@ -217,5 +217,5 @@ def api_clear():
         }), 500
 
 if __name__ == '__main__':
-    # Run development server
-    app.run(host='127.0.0.1', port=5000, debug=False, use_reloader=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
